6bd3388ffa6b2bb2328cead85e6126259420f19c
Sk0ut <fcouto1995@gmail.com>
2017-06-12 14:00:30 +0100

    New map

--- Assets/Sources/Logic/Game/Systems/InitializeGameSystem.cs
+++ Assets/Sources/Logic/Game/Systems/InitializeGameSystem.cs
@@ -17,10 +17,10 @@ public class InitializeGameSystem : IInitializeSystem
         camera.AddAsset(_context.assets.value.Camera);
         camera.AddPosition(2, 0);
         
-		var player = _context.CreatePlayer(new UnityEngine.Vector2 (3, 3));
+		var player = _context.CreatePlayer(new UnityEngine.Vector2 (6, 4));
 		_context.CreateLight (true, _context.globals.value.PlayerLightingRadius, player);
         
-		var player2 = _context.CreatePlayer(new UnityEngine.Vector2 (5, 3));
+		var player2 = _context.CreatePlayer(new UnityEngine.Vector2 (8, 6));
         player2.ReplaceActorSpeed(0.25f);
 		player2.AddAIControl (AIControlType.RANDOM);
 		_context.CreateLight (true, _context.globals.value.PlayerLightingRadius, player2);
