30eb58ab225827a0cd31564c0211354d5e76eb0f
quartz55 <john.razor97@gmail.com>
2017-06-20 23:46:25 +0100

    Added main menu

---
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGameHandler()
+    {
+        SceneManager.LoadScene("Entitas");
+    }
+
+    public void QuitHandler()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+         #endif
+    }
+}
